22b0f1655c1287dce667bd821b85e53d2e0ad6c2
riva <rivasilvercrown@gmail.com>
2021-12-03 21:23:32 +0100

    adding some code

--- day2/day2.py
+++ day2/day2.py
@@ -1,10 +1,25 @@
 #first puzzle
-def first():
-    return null
+def first(orders):
+    horizontal = 0
+    depth = 0
+    l = len(orders)
+    i = 0
+    while i < l:
+        print(orders[i])
+        i = i+1
+    pos = horizontal * depth
+    return pos
+
+def new_func():
+    return 1
 
 #second puzzle
-def second():
-    return null
+def second(orders):
+    return 0
 
 
 #################
+input_file = open("day2input.txt", "r")
+content = input_file.read()
+orders = content.split("\n")
+print(first(orders))
