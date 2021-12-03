d11bc2488cfaf92c9e34a62a87dd133ebe06a488
riva <rivasilvercrown@gmail.com>
2021-12-03 21:35:01 +0100

    second puzzle solved

--- day2/day2.py
+++ day2/day2.py
@@ -18,6 +18,23 @@ def first(orders):
 
 #second puzzle
 def second(orders):
+    horizontal = 0
+    depth = 0
+    aim = 0
+    l = len(orders)
+    i = 0
+    while i < l:
+        ins = orders[i].split()
+        if ins[0] == "forward":
+            horizontal = horizontal + int(ins[1])
+            depth = depth + (aim * int(ins[1]))
+        elif ins[0]== "up":
+            aim = aim - int(ins[1])
+        else:
+            aim = aim + int(ins[1])
+        i = i+1
+    pos = horizontal * depth
+    return pos
     return 0
 
 
@@ -25,4 +42,7 @@ def second(orders):
 input_file = open("day2input.txt", "r")
 content = input_file.read()
 orders = content.split("\n")
+print("First puzzle result:")
 print(first(orders))
+print("Second puzzle result:")
+print(second(orders))
