03d9a9f8bc6b7bae0182c8c66b8efe9e1182dd2a
riva <rivasilvercrown@gmail.com>
2021-12-03 22:23:30 +0100

    first puzzle

--- day3/day3.py
+++ day3/day3.py
@@ -1,7 +1,37 @@
 #first puzzle
 def first(values):
+    l = len(values[0])
+    i = 0
 
-    return 1
+    zeros = [0] * l
+    
+    ones = [0] * l
+    
+    gamma = ""
+    epsilon = ""
+    for value in values:
+        j = 0
+        for digit in value:
+            if digit=="1":
+                ones[j]=ones[j]+1
+            else:
+                zeros[j]=zeros[j]+1
+            j = j+1
+
+        i = i+1
+    pos = 0
+    while pos < l:
+        if zeros[pos] > ones[pos]:
+            gamma += "0"
+            epsilon += "1"
+        else:
+            gamma += "1"
+            epsilon += "0"
+        pos = pos + 1
+        
+    g = int(gamma,2)
+    e = int(epsilon,2)
+    return g*e
 
 #second puzzle
 def second(values):
