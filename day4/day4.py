208bf8a65c30bad85606ac70a04e6a7b63c7a205
riva <rivasilvercrown@gmail.com>
2021-12-05 13:37:40 +0100

    first commint

---
+++ day4/day4.py
@@ -0,0 +1,21 @@
+from collections import Counter
+#first puzzle
+def first(values):
+
+    return -1
+
+#second puzzle
+def second(values):
+    
+
+    return -1
+
+
+#################
+input_file = open("day4/day4input.txt", "r")
+content = input_file.read()
+values = content.split("\n")
+print("First puzzle result:")
+print(first(values))
+print("Second puzzle result:")
+print(second(values))
